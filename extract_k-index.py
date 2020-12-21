1d77e74d5cca2a5cafb3f0cfb15c6dbad819473f
swlee <dldldl2007@naver.com>
2020-12-21 17:38:33 +0900

    dealing with both .txt and .csv

--- extract_k-index.py
+++ extract_k-index.py
@@ -1,5 +1,7 @@
 import numpy as np
 import os
+import csv
+
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 
@@ -11,30 +13,39 @@ def func_2(x, a, b, c):
     return a*x**2 + b*x +c
 func = func_4 if ORDER==4 else func_2
 
-K_index_num = 3
+K_index_num = 5
 k_index_list = []
+curves = []
 
 for i in range(K_index_num):
-    f = open('data/OCT_K_linear/K-index_no.{}/K_index_{}.txt'.format(i+1, str(i+1).zfill(2))).readlines()
-
     idx = []
-    for line in f:
-        for val in line.split():
-            idx.append(val)
-    idx = np.round(np.asarray(idx, dtype=np.float32), 3)
+    f_list = os.listdir('data/OCT_K_linear/K-index_no.{}'.format(i+1))
+    for file in f_list:
+        if file.endswith('txt'):
+            f = open(os.path.join('data/OCT_K_linear/K-index_no.{}'.format(i+1), file)).readlines()
+            for line in f:
+                for val in line.split():
+                    idx.append(val)
+        elif file.endswith('csv'):
+            f = csv.reader(open(os.path.join('data/OCT_K_linear/K-index_no.{}'.format(i+1), file)))
+            for line in f:
+                idx.append(line[0])
+            idx[0] = 0
 
+    idx = np.asarray(idx, dtype=np.float32)
     popt, pcov = curve_fit(func, np.arange(0, 2048), idx)
 
     plt.scatter(np.arange(0, 2048), idx, marker='.', s=1, c='red', label='k-index')
     plt.plot(np.arange(0, 2048), func(np.arange(0, 2048), *popt), label='fitted curve')
     plt.legend()
-    plt.savefig('imgs/curve_fit_{}order_{}.jpg'.format(ORDER, str(i+1).zfill(2)))
+    plt.savefig('data/imgs/curve_fit_{}order_{}.png'.format(ORDER, str(i+1).zfill(2)))
     plt.close()
 
     k_index_list.append(popt)
+    curves.append(func(np.arange(0, 2048), *popt))
 
 k_index_list = np.asarray(k_index_list, dtype=np.float32)
-k_index_scaler = [1e13, 1e9, 1e5, 1e1, 1e2]
+k_index_scaler = [1e11, 1e7, 1e3, 1e0, 1e0]
 print(k_index_list)
 f = open('temp.txt', 'w')
 for line in k_index_list:
@@ -45,3 +56,8 @@ np.save('data/k_index_{}.npy'.format(ORDER), k_index_list)
 k_index_list = k_index_list*k_index_scaler
 print(k_index_list)
 np.save('data/k_index_{}_scaled.npy'.format(ORDER), k_index_list)
+
+for idx in curves:
+    plt.plot(np.arange(0, 2048), idx)
+plt.savefig('data/imgs/coeffs.png')
+plt.close()
